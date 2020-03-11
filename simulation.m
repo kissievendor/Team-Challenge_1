1a6fcea89f727b9942b8fdf1d2197657a4d01406
Stan <s_northman@hotmail.com>
2020-03-11 13:08:13 +0100

    grouping complete

--- simulation.m
+++ simulation.m
@@ -1,6 +1,8 @@
-datapath = "C:\Users\s_nor\OneDrive\Medical Imaging\Team Challenge\Part 2\data"
+%% Load data
 
-patients = loadpatient(datapath, 1, ["STIR", "tracts_C5L"])
+datapath = "C:\Users\s_nor\OneDrive\Medical Imaging\Team Challenge\Part 2\data";
+
+patients = loadpatient(datapath, 2, ["STIR", "tracts_C6R"]);
 
 
 
@@ -11,19 +13,24 @@ patients = loadpatient(datapath, 1, ["STIR", "tracts_C5L"])
 %stir = flip(flip(patients{1,2}{1,1},2),3);
 %c5l = flip(flip(patients{1,2}{2,1},2),3);
 stir = patients{1,2}{1,1};
-c5l = patients{1,2}{2,1};
-new_c5l = c5l;
+c = patients{1,2}{2,1};
+mask = c;
+mask_vertices = {};
+mask_vertices{168} = [];
 
 %frontback_tracts = c5l(:,:,80)
 
-sizeup = 0;
 
+%% Mask
+
+sizeup = 1;
+grouping_threshold = 0.5;
 % new_c5l = permute(new_c5l,[2 3 1]);
 for x = 1:168
    ones = [];
    for y = 1:85
       for z = 1:168
-          if (new_c5l(x,y,z) == 1)
+          if (mask(x,y,z) == 1)
              ones = [ones; [y,z]]; 
           end
       end
@@ -35,19 +42,115 @@ for x = 1:168
        top = maxs(1,2) + sizeup;
        right = maxs(1,1) + sizeup;
        bot = mins(1,2) - sizeup;
-       
+             
+       % mask_max
+       mask_vertices{x} = [left, bot]; 
        for y = left:right
            for z = bot:top
-               new_c5l(x,y,z) = 1;
+               mask(x,y,z) = 1;
+               if (stir(x,y,z) > stir(x,mask_vertices{x}(1),mask_vertices{x}(2)))
+                  mask_vertices{x} = [y,z]; 
+               end
            end
        end
+       % mask_group
+%        threshold = stir(x,mask_vertices{x}(1),mask_vertices{x}(2)) * grouping_threshold;
+%        mask_vertices{x} = [];
+%        for y = left:right
+%            for z = bot:top
+%                if (stir(x,y,z) > threshold)
+%                    mask_vertices{x} = [mask_vertices{x}; [y,z]]; 
+%                end
+%            end
+%        end
+       % mask_vertices
    end
 end
 
-new_c5l = stir .* new_c5l;
+mask = stir .* mask;
+
+roi_slice = 95;
 
 %roi = stir(:,:,81);
 %imshow(roi,[0,500]);
-per = permute(new_c5l,[3 2 1]);
-xa = per(:,:,76);
-imtool(per(:,:,76),[0 500])
+per = permute(mask,[3 2 1]);
+imtool(per(:,:,roi_slice),[0 500])
+
+temp_c5 = per(:,:,roi_slice);
+
+%% Directional thresholding
+% we search for the brightest pixel in the rectangle
+
+g = search(mask_vertices{roi_slice}, stir, roi_slice);
+
+% annemarie's code gives 8 points (T TR R BR B BL L TL)
+v = points(g, mask_vertices{roi_slice});
+
+% 168 long arrays with 8 columns of vertices (1 by 2s)
+
+% then Stan draws it as a 3D cylinder-like shape for presentation
+
+function [group] = search(start, stir, x)
+    brightness = @(yz) stir(x,yz(1),yz(2));    
+
+    group = [start];
+
+    threshold = brightness(start) * 0.5;
+
+    step(start + [1,0]);
+    
+    function step(s)
+        if (brightness(s) > threshold && ~isgroup(s))
+           group = [group; s];    
+           step(s + [1,0]);
+           step(s + [0,1]);
+           step(s + [-1,0]);
+           step(s + [0,-1]);
+        end
+    end
+
+    function [output] = isgroup(s)        
+        for member = group.'
+            if (s' == member)
+               output = 1; 
+               return;
+            end
+        end
+        output = 0;
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
