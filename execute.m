08490b92addeafb7465467dd6026c3f1e6445f24
kissievendor <54887561+kissievendor@users.noreply.github.com>
2020-03-16 21:45:16 +0100

    fixed forloop in execute

--- execute.m
+++ execute.m
@@ -6,19 +6,19 @@ patients = loadpatient(datapath, 1:16, ["tracts", "MIP_or", "STIR_or"]);
 patient = patients(1);
 patstruct = struct([]);
 
-for tract = ["C5L" "C5R" "C6L" "C6R" "C7L" "C7R"]
-    [diameter, area, truediameter, truearea] = nerveprocessing(patient, tract);
-    for i = length(tract)
-        patstruct{1,i}=tract;
-        patstruct{2,i}='diameter';
-        patstruct{3,i}=diameter;
-        patstruct{4,i}='true diameter';
-        patstruct{5,i}=truediameter;
-        patstruct{6,i}='area';
-        patstruct{7,i}=area';
-        patstruct{8,i}='true area';
-        patstruct{9,i}=truearea;
-    end
+tracts = ["C5L" "C5R" "C6L" "C6R" "C7L" "C7R"];
+
+for i = 1:length(tracts)
+    [diameter, area, truediameter, truearea] = nerveprocessing(patient, tracts(i));
+    patstruct{1,i}=tracts(i);
+    patstruct{2,i}='diameter';
+    patstruct{3,i}=diameter;
+    patstruct{4,i}='true diameter';
+    patstruct{5,i}=truediameter;
+    patstruct{6,i}='area';
+    patstruct{7,i}=area';
+    patstruct{8,i}='true area';
+    patstruct{9,i}=truearea;
     try
         w = waitforbuttonpress;
     catch
