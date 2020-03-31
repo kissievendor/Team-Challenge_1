e103502842042b80f0e403ae9c535bf20d87fd14
AnnemarieKoop <59692116+AnnemarieKoop@users.noreply.github.com>
2020-03-31 12:31:50 +0200

    update main.m

--- main.m
+++ main.m
@@ -1,3 +1,4 @@
-datapath = "C:\Users\s_nor\OneDrive\Medical Imaging\Team Challenge\Part 2\data";
+%datapath = "C:\Users\s_nor\OneDrive\Medical Imaging\Team Challenge\Part 2\data";
+datapath = "C:\Users\User\Documents\Medical_Imaging\Team_Challenge\TC_data\data";
 
-result = intensity(datapath, 1:2, 'threshold', 0.5, 'margin', 1);
+result = intensity(datapath, 2, 'threshold', 0.5, 'margin', 1);
