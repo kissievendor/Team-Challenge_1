f5c734fc06ee97eae211a97f20a629a9aea6223e
AnnemarieKoop <59692116+AnnemarieKoop@users.noreply.github.com>
2020-03-31 12:34:45 +0200

    Revert "update main.m"
    This reverts commit e103502842042b80f0e403ae9c535bf20d87fd14.

--- main.m
+++ main.m
@@ -1,4 +1,3 @@
-%datapath = "C:\Users\s_nor\OneDrive\Medical Imaging\Team Challenge\Part 2\data";
-datapath = "C:\Users\User\Documents\Medical_Imaging\Team_Challenge\TC_data\data";
+datapath = "C:\Users\s_nor\OneDrive\Medical Imaging\Team Challenge\Part 2\data";
 
-result = intensity(datapath, 2, 'threshold', 0.5, 'margin', 1);
+result = intensity(datapath, 1:2, 'threshold', 0.5, 'margin', 1);
